3ea70acc2b73a88edc95d62b41b569de91b34658
Tawing dela Rama <yabuking84@gmail.com>
2021-09-07 19:42:38 +0400

    genre test and updated jest command in package.json

--- tests/integration/genre.test.js
+++ tests/integration/genre.test.js
@@ -1,4 +1,8 @@
+import expectCt from 'helmet/dist/middlewares/expect-ct';
+import request from 'supertest';
 import app from '../../index.js';
+import genreModel from '../../model/genre.js';
+
 let server;
 
 describe('/api/genres', ()=>{
@@ -10,8 +14,18 @@ describe('/api/genres', ()=>{
     });
 
     describe('GET / ',()=>{
-        it('should return all genres',()=>{
+        it('should return all genres', async ()=>{
+            const Genre = genreModel.Model;
+            await Genre.collection.insertMany([
+                {name: "genre1"},
+                {name: "genre2"},
+                {name: "genre3"}
+            ]);
 
+            const response = await request(server).get('/api/genres');
+
+            expect(response.status).toBe(200);
+            expect(response.body).length.toBe(200);
         });
     });
 });
